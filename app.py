a8675a14f0f8bbc70cb1b1373550f6d549821472
Jair Torezone <jairtorezone@gmail.com>
2024-09-26 20:02:28 -0300

    add test_get_task and test_get_tasks

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def create_task():
 
   tasks.append(new_task)
   print(tasks)
-  return jsonify({"message": "Nova tarefa criada com sucesso! viva!"})
+  return jsonify({"message": "Nova tarefa criada com sucesso! viva!", "id": new_task.id})
 
 @app.route("/tasks", methods=['GET'])
 def get_taskas():
